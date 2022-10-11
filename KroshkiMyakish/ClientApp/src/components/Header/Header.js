047f7aa5dcf97dbf55024e3c7689d494567c0218
G-genius <gamelob25@gmail.com>
2022-10-11 14:28:27 +0300

    Доработка header

---
+++ KroshkiMyakish/ClientApp/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css"
+const Header = () => {
+    return (
+        <header>
+            <h1>КрошкиМякиш</h1>
+            <nav><a>First Link</a><a>Second Link</a><a>Third Link</a></nav>
+        </header>
+    )
+}
+export default Header
--- KroshkiMyakish/ClientApp/src/components/Layout.js
+++ KroshkiMyakish/ClientApp/src/components/Layout.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import React, {Component} from 'react';
 import Home from "./Home";
+import Header from "./Header/Header";
 
 export class Layout extends Component {
-  static displayName = Layout.name;
-
-  render() {
-    return (
-      <div>
-        <Container>
-          {this.props.children}
-        </Container>
-          <Home/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Home/>
+            </div>
+        );
+    }
 }
