c44977154ce64fb0653b5277e437de8bd8145a3c
G-genius <gamelob25@gmail.com>
2022-10-27 10:43:12 +0300

    Верстка личного кабинета

--- KroshkiMyakish/ClientApp/src/Pages/AccountPage/AccountPage.js
+++ KroshkiMyakish/ClientApp/src/Pages/AccountPage/AccountPage.js
@@ -1,32 +1,40 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import UserLogo from "../../img/header/user.svg"
+import "./AccountPage.css"
+
 const AccountPage = () => {
     return (
-        <div className="account_page">
+        <div>
             <Header/>
-                <div className="container">
-                    <div className="account_info">
-                        <img src={UserLogo} alt="user-logo"/>
-                        <h1 className="user_name">Имя Аккаунта</h1>
+            <div className="container">
+                <div className="account_main_page">
+                    <div className="account_page">
+                        <div className="account_info">
+                            <img src={UserLogo} alt="user-logo"/>
+                            <div className="user_locate">
+                                <h1 className="user_name">Имя Аккаунта</h1>
+                                <span>Страна, город</span>
+                            </div>
+                        </div>
+                        <div className="account-block">
+                            <span>Интересы:</span>
+                        </div>
+                        <div className="account-block">
+                            <span>Обо мне:</span>
+                        </div>
+                        <div className="account-block">
+                            <span>Любимые блюда:</span>
+                        </div>
+                        <div className="account-block">
+                            <span>Добавлено рецептов: {}</span>
+                        </div>
                     </div>
-                    <div className="user_locate">
-                        <span>Страна</span>
-                        <span>Город</span>
-                    </div>
-                    <div className="interests">
-                        <span>Интересы:</span>
-                    </div>
-                    <div className="about">
-                        <span>Обо мне:</span>
-                    </div>
-                    <div className="love_food">
-                        <span>Любимые блюда:</span>
-                    </div>
-                    <div className="recipes_count">
-                        <span>Добавлено рецептов: {}</span>
+                    <div className="change_info">
+                        <a>Редактировать профиль</a>
                     </div>
                 </div>
+            </div>
             <Footer/>
         </div>
     )
