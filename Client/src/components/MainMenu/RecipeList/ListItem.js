d451b0900a261819687b9d22c903b5c7ddd20ebb
G-genius <gamelob25@gmail.com>
2023-03-20 21:17:28 +0300

    Доработка интерфейса

--- Client/src/components/MainMenu/RecipeList/ListItem.js
+++ Client/src/components/MainMenu/RecipeList/ListItem.js
@@ -9,6 +9,7 @@ import FileBase64 from "react-filebase64";
 
 const ListItem = () => {
     const [recipes, setRecipes] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
     const OpenRecipe = () => {
         window.scrollBy({
             top: -10000,
@@ -20,33 +21,48 @@ const ListItem = () => {
         try {
             const response = await RecipeService.getRecipes();
             setRecipes(response.data);
+            setIsLoading(true)
         } catch (e) {
             console.log(e);
         }
     }
 
-    getRecipes()
-    return (
-        <div className="list-item">
-            {recipes.map(recipe =>
-                <div className="recipe-item" key={recipe._id}>
-                    <Link to={"/recipe/"+recipe._id} className="link" onClick={OpenRecipe}>
-                        <img src={recipe.mainPhoto} alt="dish1"/>
-                        <p>{recipe.name}</p>
-                        <div className="recipe-bottom-list">
-                            <div className="recipe-bottom">
-                                <img src={watch} alt="dish2"/>
-                                <p>{recipe.time} мин.</p>
-                            </div>
-                            {/*<div className="recipe-bottom">*/}
-                            {/*    <img src={chat} alt="dish3"/>*/}
-                            {/*    <p>35</p>*/}
-                            {/*</div>*/}
-                        </div>
-                    </Link>
-                </div>
-            )}
+    setTimeout(() => {
+        if (isLoading) {
+            const loading = document.getElementById("loader")
+            loading.style.display = "none"
+        }
+        getRecipes()
+    }, 2)
 
+    return (
+        <div>
+            <div className="loader" id="loader">
+                <div className="inner one"></div>
+                <div className="inner two"></div>
+                <div className="inner three"></div>
+            </div>
+            <div className="list-item">
+                {recipes.map(recipe =>
+                    <div className="recipe-item" key={recipe._id}>
+                        <Link to={"/recipe/" + recipe._id} className="link" onClick={OpenRecipe}>
+                            <img src={recipe.mainPhoto} alt="dish1"/>
+                            <p>{recipe.name}</p>
+                            <div className="recipe-bottom-list">
+                                <div className="recipe-bottom">
+                                    <img src={watch} alt="dish2"/>
+                                    <p>{recipe.time} мин.</p>
+                                </div>
+                                {/*<div className="recipe-bottom">*/}
+                                {/*    <img src={chat} alt="dish3"/>*/}
+                                {/*    <p>35</p>*/}
+                                {/*</div>*/}
+                            </div>
+                        </Link>
+                    </div>
+                )}
+
+            </div>
         </div>
     )
 }
